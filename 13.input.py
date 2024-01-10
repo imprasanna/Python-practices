3fdca9fddc5f7396e073e586491f517c2b1ac799
Prasanna Acharya <prasannaacharya2073@gmail.com>
2024-01-10 21:39:38 +0545

    input practice file added

---
+++ 13.input.py
@@ -0,0 +1,14 @@
+test = input("Enter the IP: ")
+print(test)
+
+print("----------")
+
+while True:
+    test = input("Enter the IP address: ")
+    print(">>> {}".format(test))
+    if test == "exit":
+        break
+    else:
+        print("exploiting...") 
+        print("Enter \"exit\" to exit") 
+    
